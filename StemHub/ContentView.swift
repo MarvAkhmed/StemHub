e6bab370424dc18abdedb986fdb9f2825bdc5d1b
Marwa <MarvAkhmed@stud.kpfu.ru>
2026-04-05 02:00:17 +0300

    Initial Commit

---
+++ StemHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StemHub
+//
+//  Created by Marwa Awad on 05.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StemHub/StemHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  StemHubApp.swift
+//  StemHub
+//
+//  Created by Marwa Awad on 05.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct StemHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
